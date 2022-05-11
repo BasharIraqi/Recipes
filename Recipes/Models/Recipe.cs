1778c9cc250081411ca900f5fbb42748252d6758
BasharIraqi <bashar.oroq@hotmail.com>
2022-05-11 19:42:08 +0300

    Add project files.

---
+++ Recipes/Models/Recipe.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string CakeName { get; set; }
+        public string CakeRecipe { get; set; }
+    }
+}
